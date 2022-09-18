b73db93b08b46036b685f423de3b1ad3c20b60c4
DarekLabaj <dareklabaj@gmail.com>
2022-09-18 10:16:48 +0200

    Cleaning main function

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
-# import Starfield
-# import TheSnakeGame
-# import PurpleRain
+import sys
+import Starfield
+import TheSnakeGame
+import PurpleRain
 import SpaceInvadersClone
-# import MathematicalRosePatterns
-# import BrownianTreeSnowflake
+import MathematicalRosePatterns
+import BrownianTreeSnowflake
 
 
 if __name__ == "__main__":
-    # window = Starfield.Window(800, 800, "Coding Challange #1: Starfield", 60)
-    # window = TheSnakeGame.Window(600, 600, "Coding Challange #3: The Snake Game", 10)
-    # window = PurpleRain.Window(640, 360, "Coding Challange #4: Purple Rain", 60)
-    window = SpaceInvadersClone.Window(600, 400, "Coding Challange #5: Space Invaders", 60)
-    # window = MathematicalRosePatterns.Window(800, 800, "Coding Challange #55: Mathematical Rose Patterns", 60)
-    # window = BrownianTreeSnowflake.Window(600, 600, "Coding Challange #127: Brownian Tree Sonwflake")
-    window.Run()
+    args = sys.argv
+    if len(args) > 1 and args[1].isdigit():
+        challange = int(args[1])
+    else:
+        challange = 1
+    match challange:
+        case 1:
+            window = Starfield.Window(
+                800, 800, f"Coding Challange #{challange}: Starfield", 60
+            )
+        case 3:
+            window = TheSnakeGame.Window(
+                600, 600, f"Coding Challange #{challange}: The Snake Game", 10
+            )
+        case 4:
+            window = PurpleRain.Window(
+                640, 360, f"Coding Challange #{challange}: Purple Rain", 60
+            )
+        case 5:
+            window = SpaceInvadersClone.Window(
+                600, 400, f"Coding Challange #{challange}: Space Invaders", 60
+            )
+        case 55:
+            window = MathematicalRosePatterns.Window(
+                800,
+                800,
+                f"Coding Challange #{challange}: Mathematical Rose Patterns",
+                60,
+            )
+        case 127:
+            window = BrownianTreeSnowflake.Window(
+                600, 600, f"Coding Challange #{challange}: Brownian Tree Sonwflake"
+            )
+    if window:
+        window.Run()
