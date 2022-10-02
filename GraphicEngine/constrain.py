27c23a27f9c2cfecc024d9e74c2ce5de1cf8d83e
DarekLabaj <dareklabaj@gmail.com>
2022-10-02 08:04:35 +0200

    Cleanup and moving GraphicEngine to other location

--- GraphicEngine/constrain.py
+++
@@ -1,6 +0,0 @@
-def constrain(value: float, lowLimit: float, highLimit: float):
-    if value <= lowLimit:
-        return lowLimit
-    if value >= highLimit:
-        return highLimit
-    return value
