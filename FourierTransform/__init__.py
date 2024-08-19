8f6616221f69c2db179847341ce6e2dbd6a56cd4
DarekLabaj <dareklabaj@gmail.com>
2024-08-19 13:11:39 +0200

    added fft adn bg original signal for comparison

--- FourierTransform/__init__.py
+++ FourierTransform/__init__.py
@@ -1,4 +1,6 @@
 from math import atan2, cos, pi, sin, sqrt
+import numpy as np
+import time
 
 from attr import dataclass
 from FourierTransform import drawing
@@ -19,39 +21,44 @@ class Window(GraphicEngine.PygameGFX):
     path: list[tuple[float, float]] = []
     signalY: list[float] = []
     signalX: list[float] = []
+    signalPath: list[tuple[float, float]] = []
     fourierY: list[FourierData] = []
     fourierX: list[FourierData] = []
-    skip = 8
+    skip = 4
 
     def Setup(self):
         self.background(0)
         self.signalX = [drawing.drawing[i].x for i in range(0, len(drawing.drawing), self.skip)]
         self.signalY = [drawing.drawing[i].y for i in range(0, len(drawing.drawing), self.skip)]
-        self.fourierX = self.dft(self.signalX)
-        self.fourierY = self.dft(self.signalY)
+        self.signalPath = [(drawing.drawing[i].x, drawing.drawing[i].y)
+                           for i in range(0, len(drawing.drawing), self.skip)]
+        startCalculationTime = time.time()
+        # self.fourierX = self.dft(self.signalX)
+        # self.fourierY = self.dft(self.signalY)
+        self.fourierX = self.fft(self.signalX)
+        self.fourierY = self.fft(self.signalY)
+        print(f"Total Calculation Time: {(time.time()-startCalculationTime)*1000:.3f} [ms]")
         self.fourierX.sort(reverse=True, key=lambda val: val.amp)
         self.fourierY.sort(reverse=True, key=lambda val: val.amp)
 
     def epicycle(self, x: float, y: float, rotation: float, fourier: list[FourierData]):
+        self.stroke((255, 255, 255, 192))
         radius = 0
         for val in fourier:
             prevX = x
             prevY = y
-
             freq = val.freq
             radius = val.amp
             phase = val.phase
             x += radius * cos(freq * self.time + phase + rotation)
             y += radius * sin(freq * self.time + phase + rotation)
-
-            self.stroke((255, 255, 255, 20))
             self.noFill()
             self.ellipse((prevX, prevY, radius*2, radius*2))
             self.line((prevX, prevY), (x, y))
         return x, y
 
     def Draw(self):
-        self.background(0)
+        self.background(40)
         # self.translate(150, 200)
 
         x1, y1 = self.epicycle(self.Width/2+100, 100, 0, self.fourierX)
@@ -60,13 +67,17 @@ class Window(GraphicEngine.PygameGFX):
         self.path.insert(0, (x1, y2))
 
         # self.translate(200, 0)
+        self.stroke((255, 255, 255, 40))
         self.line((x1, y1), (self.path[0][0], self.path[0][1]))
         self.line((x2, y2), (self.path[0][0], self.path[0][1]))
         self.noFill()
-        self.polygon(
-            [(x[0], x[1]) for x in self.path]
-        )
-
+        self.push()
+        self.translate(500, 380)
+        self.stroke((0, 255, 255, 60))
+        self.polygon(self.signalPath)
+        self.pop()
+        self.stroke((255, 255, 255, 255))
+        self.polygon(self.path)
         if (len(self.path) > len(drawing.drawing)/self.skip-10):
             self.path.pop()
 
@@ -90,4 +101,20 @@ class Window(GraphicEngine.PygameGFX):
             amp = sqrt(re*re+im*im)
             phase = atan2(im, re)
             X[k] = FourierData(re, im, freq, amp, phase)
+        return X
+
+    @staticmethod
+    def fft(x: list[float]) -> list[FourierData]:
+        N = len(x)
+        X_complex = np.fft.fft(x)
+        X = [
+            FourierData(
+                re=np.real(X_complex[k])/N,
+                im=np.imag(X_complex[k])/N,
+                freq=k,
+                amp=np.abs(X_complex[k])/N,
+                phase=np.angle(X_complex[k])
+            )
+            for k in range(N)
+        ]
         return X
